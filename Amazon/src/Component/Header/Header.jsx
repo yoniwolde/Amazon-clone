78bc69bf87fb297fdf2e4e161510428fbd13872f
yoniwolde <yoniwol89@gmail.com>
2024-09-16 16:12:22 -0400

    Header

--- Amazon/src/Component/Header/Header.jsx
+++ Amazon/src/Component/Header/Header.jsx
@@ -3,63 +3,69 @@ import "./Header.css";
 import { SlLocationPin } from "react-icons/sl";
 import { BsSearch } from "react-icons/bs";
 import { BiCart } from "react-icons/bi";
+import LowerHeader from './LowerHeader';
 
 function Header() {
   return (
-    <section className='header_container'>
+    <>
       <section>
-        <div className='logo_container'>
+        <div className="header_container">
           {/*logo*/}
-          <a href="#">
-            <img src="../../assets/Image/Amazoncom-yellow-arrow.png" alt="amazon logo" />
-          </a>
-          {/*delivery*/}
-          <span><SlLocationPin/></span>
-          <div>
-            <p>Delivered to</p>
-            <span>Ethiopia</span>
-          </div>
-        </div>
-        <div>
-          {/*search*/}
-          <select name="" id="">
-            <option value="">All</option>
-          </select>
-          <input type="text" name="" id="" placeholder="search Product" />
-          <BsSearch/>
-        </div>
-        {/* right side link*/}
+          <div className="logo_container">
+            <a href="#">
+              <img
+                src="https://wallpapers.com/images/hd/amazon-logo-black-background-xb9pdemosnjfz9ej.jpg"
+                alt="amazon logo"
+              />
+            </a>
+            <div className="delivery">
+              <span>
+                <SlLocationPin />
+              </span>
               <div>
-                  <div>
-                      <div>
-                          <img src='../../assets/Image/Flag_of_the_United_States.png' alt='US flag' />
-                          <section>
-                              <option value="">
-                                  EN
-                              </option>
-                          </section>
-                      </div>
-                      {/* three components*/}
-                      <a href="">
-                          <div>
-                              <p>Sign In</p>
-                              <span>Account & Lists</span>
-                          </div>
-                      </a>
-                      {/*orders */}
-                      <a href=''>
-                          <p>Returns</p>
-                          <span>& Orders</span>
-                      </a>
-                      {/*cart*/}
-                      <a to="/cart">
-                          <BiCart/>
-                          <span>0</span>
-                      </a>
-                  </div>
-        </div>
+                <p>Delivered to</p>
+                <span>Ethiopia</span>
+              </div>
+            </div>
+          </div>
+          <div className="search">
+            <select name="" id="">
+              <option value="">All</option>
+            </select>
+            <input type="text" name="" id="" placeholder="search Product" />
+            <BsSearch size={25} />
+          </div>
+          {/* right side link*/}
+            <div className='order_container'>
+              <a href='' className='language'>
+                <img
+                  src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Flag_of_the_United_States_%28DoS_ECA_Color_Standard%29.svg/255px-Flag_of_the_United_States_%28DoS_ECA_Color_Standard%29.svg.png"
+                  alt="US flag"
+                />
+                <select name="" id="">
+                  <option value="">EN</option>
+                </select>
+              </a>
+              {/* three components*/}
+              <a href="">
+                <div>
+                  <p>Sign In</p>
+                  <span>Account & Lists</span>
+                </div>
+              </a>
+              <a href="">
+                <p>Returns</p>
+                <span>& Orders</span>
+              </a>
+              <a href='' className='cart' >
+                <BiCart size={35} />
+                <span>0</span>
+              </a>
+            </div>
+          </div>
       </section>
-    </section>
+      <LowerHeader/>
+    </>
   );
 }
 
---
+++ Amazon/src/Component/Header/LowerHeader.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { AiOutlineMenu } from "react-icons/ai";
+
+function LowerHeader() {
+  return (
+    <div className='lower_container'>
+      <ul>
+        <li>
+          <AiOutlineMenu />
+          <p>All</p>
+        </li>
+        <li>Today's Deals</li>
+        <li>Costumer Service</li>
+        <li>Registry</li>
+        <li>Gift Cards</li>
+        <li>Sell</li>
+      </ul>
+    </div>
+  );
+}
+
+export default LowerHeader
